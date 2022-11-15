1233027451319f8f16575e5bebab458c9e4c711a
Andhika <andhika.ardhiansyah19@gmail.com>
2022-11-15 08:51:09 +0700

    update

--- routes/product.go
+++ routes/product.go
@@ -13,7 +13,7 @@ func ProductRoutes(r *mux.Router) {
 	productRepository := repositories.RepositoryProduct(mysql.DB)
 	h := handlers.HandlerProduct(productRepository)
 
-	r.HandleFunc("/products", middleware.Auth(h.FindProducts)).Methods("GET")
+	r.HandleFunc("/products", h.FindProducts).Methods("GET")
 	r.HandleFunc("/product/{id}", h.GetProduct).Methods("GET")
 	r.HandleFunc("/product", middleware.Auth(middleware.UploadFile(h.CreateProduct))).Methods("POST")
 	r.HandleFunc("/product/{id}", middleware.Auth(middleware.UploadFile(h.UpdateProduct))).Methods("PATCH")
